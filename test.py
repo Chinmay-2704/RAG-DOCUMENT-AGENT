33b1a3f59a2152e713d62ef53f36be3b12d08252
Chinmay-2704 <143205225+Chinmay-2704@users.noreply.github.com>
2026-06-01 23:46:41 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("GROQ_API_KEY =", os.getenv("GROQ_API_KEY"))
-print("TAVILY_API_KEY =", os.getenv("TAVILY_API_KEY"))
-print("GROQ_MODEL =", os.getenv("GROQ_MODEL"))
